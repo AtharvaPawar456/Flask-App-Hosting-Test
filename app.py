39cd447b211c29a0f9260edebe1c2ea796d134b7
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2023-04-27 18:01:32 +0530

    stremlit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import streamlit as st
 
 app = Flask(__name__)
 
@@ -7,5 +9,11 @@ def index():
     username = 'John'  # Replace with your desired username
     return render_template('index.html', username=username)
 
+    return render_template('index.html')
+@app.route('/streamlit')
+def streamlit():
+    st.set_page_config(page_title="My Streamlit App")
+    st.write("Hello, world!")
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
+    app.run()
